9bfacd87b5f50d3919af22ab77eb0bdbf963af76
Jared Rewerts <jaredrewerts@gmail.com>
2018-08-10 15:49:25 -0600

    Added a user avatar library.

--- my-app/src/components/User.jsx
+++ my-app/src/components/User.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import Avatar from 'react-avatar';
 
 const cardStyle = {
     backgroundColor: 'white',
     borderRadius: '4px',
     boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
-    width: '300px'
+    width: '300px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
 }
 
 class User extends React.Component {
     render() {
         return <div>
             <div style={cardStyle}>
+            <Avatar size="100" name={this.props.name} src={this.props.avatarUrl} />
                 Hello, {this.props.name}
             </div>
         </div>;
