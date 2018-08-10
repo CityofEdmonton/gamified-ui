54ea9e16d3dbe7237dcf897b2bfa10259753d443
Jared Rewerts <jaredrewerts@gmail.com>
2018-08-10 16:04:43 -0600

    Added some additional example info.

--- my-app/src/components/User.jsx
+++ my-app/src/components/User.jsx
@@ -17,12 +17,18 @@ const avatarStyle = {
     overflow: 'hidden'
 }
 
+const descStyle = {
+    textAlign: 'left',
+    margin: '16px 8px'
+}
+
 class User extends React.Component {
     render() {
         return <div>
             <div style={cardStyle}>
-            <Avatar size="100" style={avatarStyle} name={this.props.name} src={this.props.avatarUrl} />
-                Hello, {this.props.name}
+                <Avatar size="100" style={avatarStyle} name={this.props.name} src={this.props.avatarUrl} />
+                <h3>Hello, {this.props.name}</h3>
+                <div style={descStyle}>There is a ton of info about this individual! Text goes here and fills up space so it looks nice.</div>
             </div>
         </div>;
     }
